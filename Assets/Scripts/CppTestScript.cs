7f39817342dccd02abc3e003db2baa6cb48026ee
わだたける <68636852+wdtkr@users.noreply.github.com>
2023-09-02 03:02:52 +0900

    コールバック関数の実装と呼び出し可能

--- Assets/Scripts/CppTestScript.cs
+++ Assets/Scripts/CppTestScript.cs
@@ -7,16 +7,27 @@ using UnityEngine;
 public class CppTestScript : MonoBehaviour
 {
     [DllImport("lib")]
-    private static extern int sum(int a, int b);
-    [DllImport("lib")]
     private static extern IntPtr registerFqdn(string str);
     [DllImport("lib")]
     private static extern IntPtr sendBinaryData(string str);
+    [DllImport("lib")]
+    private static extern void setCallback(CallbackDelegate callback);
+
+    [DllImport("lib")]
+    private static extern void triggerCallback();
+    
+    // コールバックのデリゲート型定義
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate void CallbackDelegate(string message);
+
+    [AOT.MonoPInvokeCallback(typeof(CallbackDelegate))]
+    public static void MyCallback(string message)
+    {
+        Debug.Log("Called from C++ with message: " + message);
+    }
 
     void Start()
     {
-        Debug.Log(sum(1,2));
-        
         var ptr = registerFqdn("test");
         string result = Marshal.PtrToStringAnsi(ptr);
         Debug.Log(result);
@@ -24,6 +35,12 @@ public class CppTestScript : MonoBehaviour
         var ptr2 = sendBinaryData("test");
         string result2 = Marshal.PtrToStringAnsi(ptr2);
         Debug.Log(result2);
+        
+        // C++のコールバックをC#の関数で設定
+        setCallback(MyCallback);
+
+        // C++のコールバックをトリガー
+        triggerCallback();
 
         Debug.Log("debug終了");
     }
