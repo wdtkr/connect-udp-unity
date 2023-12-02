3ef792a550ed033f30416446cae55de6cef69908
わだたける <68636852+wdtkr@users.noreply.github.com>
2023-12-02 21:29:10 +0900

    カメラの送信と受信、それぞれで実装

--- Assets/Scripts/UdpScript.cs
+++ Assets/Scripts/UdpScript.cs
@@ -4,16 +4,20 @@ using System.Collections.Generic;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Threading.Tasks;
+using Palmmedia.ReportGenerator.Core.Parser.Analysis;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class UdpScript : MonoBehaviour
 {
     public Button startReceiveButton;
     public Button endReceiveButton;
     public Button sendButton;
     public Button startVideoButton;
+
+    public Button receiveTestVideoButton;
+    public Button appStopButton;
 
     public Text peerFqdnText;
     public Text receiveTextLog;
@@ -32,8 +36,10 @@ public class UdpScript : MonoBehaviour
     private SingleAssignmentDisposable _disposable = new SingleAssignmentDisposable();
     
     private Texture2D _textureTmp;
-
-    // C++のライブラリから呼び出す関数の宣言
+    
+    /* =============================
+     * UDPライブラリから呼び出す関数の宣言
+     ============================= */
     [DllImport("udp-lib")]
     private static extern void setCallback(DebugCallbackDelegate debugCallback, CallbackDelegate receiveCallback,
         DebugCallbackDelegate startCallback);
@@ -49,6 +55,31 @@ public class UdpScript : MonoBehaviour
 
     [DllImport("udp-lib")]
     private static extern void socketClose();
+    
+    /* =============================
+     * Mediaライブラリから呼び出す関数の宣言
+     ============================= */
+    
+    [DllImport("media-lib")]
+    private static extern void setCallback(DebugCallbackDelegate debugCallback, CallbackDelegate receiveCallback);
+    
+    [DllImport("media-lib")]
+    private static extern void setLibraryPath(string libraryPath);
+    
+    [DllImport("media-lib")]
+    private static extern int initEncodeVideoData(int videoFormat);
+    [DllImport("media-lib")]
+    private static extern void encodeVideoData(byte[] inputData,int length);
+    [DllImport("media-lib")]
+    private static extern void destroyEncoder();
+    [DllImport("media-lib")]
+    private static extern void initDecodeVideoData();
+    [DllImport("media-lib")]
+    private static extern void receiveAndDecodeVideoData();
+    [DllImport("media-lib")]
+    private static extern void destroyDecoder();
+    [DllImport("media-lib")]
+    private static extern void test();
 
     // コールバックのデリゲート型定義
     [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
@@ -68,13 +99,24 @@ public class UdpScript : MonoBehaviour
     private static Subject<string> _staticMessageSubject = new Subject<string>();
     private static Subject<byte[]> _staticVideoSubject = new Subject<byte[]>();
     
+    private Texture2D _receivedTexture = new Texture2D(1920, 1080, TextureFormat.RGBA32, false);
+    
     private static object _staticLock = new object();
     private static SynchronizationContext _mainThread;
 
     private List<string> _messageLog = new List<string>() { };
-
+    
+    private int switchFlag = 0;
+    private bool videoStreamFlag = false;
+    private bool stringStreamFlag = false;
+    
     private void Start()
     {
+        // test
+        setCallback(DebugCallback, ReceiveTestVideo);
+        setLibraryPath("./Assets/Plugins/CppConnect/libopenh264-2.3.1-mac-arm64.dylib");
+        test();
+        
         _mainThread = SynchronizationContext.Current;
 
         receiveTextLog.text = "";
@@ -82,20 +124,34 @@ public class UdpScript : MonoBehaviour
 
         sendButton.onClick.AddListener(() => SendStringData(messageInputField.text));
         // sendButton.onClick.AddListener(() => SendData(messageInputField.text));
-        startReceiveButton.onClick.AddListener(StartReceiveLoop);
+        startReceiveButton.onClick.AddListener(() =>
+        {
+            stringStreamFlag = true;
+            StartReceiveLoop();
+        });
+        appStopButton.onClick.AddListener(TestStop);
         endReceiveButton.onClick.AddListener(() =>
         {
-            _receiveThread.Abort();
+            if (switchFlag != 1) return;
+            _receiveThread?.Abort();
             socketClose();
             Debug.Log("停止ボタンによるスレッド停止。");
         });
         
         startVideoButton.onClick.AddListener(() =>
         {
+            switchFlag = 2;
             stringTestPanel.SetActive(false);
             videoTestPanel.SetActive(true);
-            
             StartCoroutine(StartVideoStream());
+        });
+        
+        receiveTestVideoButton.onClick.AddListener(() =>
+        {
+            switchFlag = 3;
+            stringTestPanel.SetActive(false);
+            videoTestPanel.SetActive(true);
+            ReceiveTestStartVideoStream();
         });
 
         _staticMessageSubject.ObserveOnMainThread()
@@ -114,24 +170,43 @@ public class UdpScript : MonoBehaviour
         _staticVideoSubject.ObserveOnMainThread()
             .Subscribe(data =>
             {
-                // 画像データをTexture2Dとしてロード
-                Texture2D receivedTexture = new Texture2D(2, 2);
-                receivedTexture.LoadImage(data); // receivedDataはbyte[]
-                
-                peerVideoCapture.texture = receivedTexture;
+                _receivedTexture.LoadRawTextureData(data);
+                _receivedTexture.Apply();
+                peerVideoCapture.texture = _receivedTexture;
             })
             .AddTo(this);
 
         // SetFqdn();
     }
 
-    private void OnApplicationQuit()
+    private async void OnApplicationQuit()
     {
-        // スレッド停止
-        _receiveThread.Abort();
-        socketClose();
-        Debug.Log("強制終了によるスレッド停止。");
-        StopVideoStream();
+        Debug.Log("終了ボタンによるスレッド停止。");
+        await CleanupApplication();
+    }
+
+    // 自作の停止関数
+    private async Task CleanupApplication()
+    {
+        _receiveThread?.Abort();
+        if(switchFlag == 1) socketClose();
+
+        // ビデオ送信開始してた場合
+        if(switchFlag == 2) StopVideoStream();
+        // ビデオ受信開始してた場合
+        if(switchFlag == 3) await Task.Run(destroyDecoder);
+    }
+
+    private void TestStop()
+    {
+        TestClean();
+    }
+    
+    private async void TestClean()
+    {
+        _disposable?.Dispose();
+        _receiveThread?.Abort();
+        await Task.Run(destroyDecoder);
     }
 
     private void SetPort(string portInp)
@@ -233,45 +308,74 @@ public class UdpScript : MonoBehaviour
 
     private IEnumerator StartVideoStream()
     {
+        Debug.Log("StartVideoStreamを開始");
+        initEncodeVideoData(1);
+        // カメラのセットアップ
         var devices = WebCamTexture.devices;
-        foreach (var device in devices)
-        {
-            Debug.Log(device.name);
-        }
-
         if (devices.Length <= 0) yield break;
-        _webCamTexture = new WebCamTexture(devices[0].name, 640, 480,30); // 幅と高さを明示的に設定
+
+        _webCamTexture = new WebCamTexture(devices[0].name, 1920, 1080, 30);
         myVideoCapture.texture = _webCamTexture;
         _webCamTexture.Play();
-        
-        // WebCamTexture.widthが100以下の状況の場合初期化が完了していないので、100以上になるまで待機
-        while (_webCamTexture.width < 100) {
+
+        // 初期化が完了するまで待機
+        while (_webCamTexture.width < 100) 
+        {
             yield return null;
         }
 
-        // カメラがスタートしてからバッファのサイズを設定
+        // バッファのサイズを設定
         _tmpBuffer = new Color32[_webCamTexture.width * _webCamTexture.height];
-        
-        StartReceiveLoop();
-        
-        _disposable.Disposable = Observable.EveryUpdate()
+        _textureTmp = new Texture2D(_webCamTexture.width, _webCamTexture.height, TextureFormat.RGBA32, false);
+
+        _disposable.Disposable = Observable.Interval(TimeSpan.FromSeconds(1.0 / 30))
             .Subscribe(_ => {
-                _webCamTexture.GetPixels32(_tmpBuffer);
-                _textureTmp = new Texture2D(_webCamTexture.height, _webCamTexture.width, TextureFormat.ARGB32, false);
-                // カメラのピクセルデータを設定
-                _textureTmp.SetPixels32(_tmpBuffer);
-                // TextureをApply
-                _textureTmp.Apply();
-                // Encode
-                byte[] bin = _textureTmp.EncodeToJPG();
-                // SendData(bin);
-                Debug.Log(BitConverter.ToString(bin));
+                CallEncodeVideoData();
             });
+    }
+    
+    private void CallEncodeVideoData()
+    {
+        // WebCamTexture を更新
+        _webCamTexture.GetPixels32(_tmpBuffer);
+        _textureTmp.SetPixels32(_tmpBuffer);
+        _textureTmp.Apply();
+
+        // peerVideoCapture.texture = _textureTmp;
+        // バイトデータを取得
+        byte[] frameData = _textureTmp.GetRawTextureData();
+
+        // C++ のエンコード関数を呼び出し
+        encodeVideoData(frameData,frameData.Length);
     }
 
     private void StopVideoStream()
     {
-        _disposable.Dispose();
+        _disposable?.Dispose();
         _webCamTexture.Stop();
+        destroyEncoder();
+    }
+
+    private void ReceiveTestStartVideoStream()
+    {
+        initDecodeVideoData();
+        _receiveThread = new Thread(new ThreadStart(ReceiveTestVideoDataLoop)) { IsBackground = true };
+        _receiveThread.Start();
+    }
+
+    private void ReceiveTestVideoDataLoop()
+    {
+        Debug.Log("C#受信開始");
+        while (true)
+        {
+            receiveAndDecodeVideoData();
+        }
+    }
+
+    [AOT.MonoPInvokeCallback(typeof(CallbackDelegate))]
+    public void ReceiveTestVideo(byte[] data, int size, int type)
+    {
+        Debug.Log("ReceiveTestVideoが呼ばれました size："+size);
+        _staticVideoSubject.OnNext(data);
     }
 }
