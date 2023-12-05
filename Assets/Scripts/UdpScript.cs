1cadabdac3e1ad5e49057923195c3414c78d2435
わだたける <68636852+wdtkr@users.noreply.github.com>
2023-12-05 13:14:35 +0900

    音声送受信途中

--- Assets/Scripts/UdpScript.cs
+++ Assets/Scripts/UdpScript.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using UniRx;
 using UnityEngine;
+using UnityEngine.Profiling;
 using UnityEngine.UI;
 public class UdpScript : MonoBehaviour
 {
@@ -21,7 +22,8 @@ public class UdpScript : MonoBehaviour
     // プライベート変数
     private WebCamTexture _webCamTexture;
     private Color32[] _tmpBuffer;
-    private SingleAssignmentDisposable _disposable = new SingleAssignmentDisposable();
+    private SingleAssignmentDisposable _sendVideoDisposable = new SingleAssignmentDisposable();
+    private SingleAssignmentDisposable _sendAudioDisposable = new SingleAssignmentDisposable();
     private Texture2D _textureTmp;
     private Thread _receiveThread, _tcpReceiveThread;
     private IEnumerator _tcpCoroutine;
@@ -31,6 +33,31 @@ public class UdpScript : MonoBehaviour
     private static SynchronizationContext _mainThread;
     private List<string> _messageLog = new List<string>() { };
     private Texture2D _receivedTexture;
+    private string _myFqdn,_micName;
+    private int _micSampleRate = 48000; // サンプルレート
+    private int _sampleSize = 960; // Opus FRAME_SIZEに合わせる
+    private AudioClip _microphoneClip;
+    private int _microphonePosition;
+    private float[] _samples = new float[960];
+    private byte[] _audioData;
+        
+    [SerializeField] private int port = 8000;
+    [SerializeField] private string peerFqdn = null;
+
+    [SerializeField] private string peerAddress = "127.0.0.1";
+    [SerializeField] private int myTcpPort = 30000;
+    [SerializeField] private int peerTcpPort = 30001;
+    [SerializeField] private int myVideoPort = 30002;
+    [SerializeField] private int peerVideoPort = 30006;
+    [SerializeField] private int myAudioPort = 30004;
+    [SerializeField] private int peerAudioPort = 30008;
+    [SerializeField] private int cameraNumber = 0;
+    [SerializeField] private int micNumber = 0;
+    
+    private int switchFlag = 0;
+    private bool videoStreamFlag = false;
+    private bool stringStreamFlag = false;
+    private bool endTcpConnect = false;
 
     /* =============================
      * UDPライブラリから呼び出す関数の宣言
@@ -56,7 +83,7 @@ public class UdpScript : MonoBehaviour
      ============================= */
     
     [DllImport("media-lib")]
-    private static extern void setCallback(DebugCallbackDelegate debugCallback, CallbackDelegate receiveCallback);
+    private static extern void setMediaCallback(DebugCallbackDelegate debugCallback, CallbackDelegate receiveVideoCallback,CallbackDelegate receiveAudioCallback);
     
     [DllImport("media-lib")]
     private static extern void setLibraryPath(string libraryPath);
@@ -70,6 +97,11 @@ public class UdpScript : MonoBehaviour
     private static extern int initEncodeVideoData(int videoFormat);
     [DllImport("media-lib")]
     private static extern void encodeVideoData(byte[] inputData,int length);
+    [DllImport("media-lib")]
+    private static extern void initEncodeAudioData();
+    [DllImport("media-lib")]
+    private static extern void encodeAudioData(byte[] audioData, int length);
+    
     [DllImport("media-lib")]
     private static extern void destroyEncoder();
     [DllImport("media-lib")]
@@ -91,32 +123,12 @@ public class UdpScript : MonoBehaviour
     private delegate void CallbackDelegate(
         [MarshalAs(UnmanagedType.LPArray, SizeParamIndex=1)] byte[] data, int size, int type);
     
-    [SerializeField] private int port = 8000;
-    [SerializeField] private string peerFqdn = null;
-    private string _myFqdn;
-    
-    [SerializeField] private string peerAddress = "127.0.0.1";
-    [SerializeField] private int myTcpPort = 30000;
-    [SerializeField] private int peerTcpPort = 30001;
-    [SerializeField] private int myVideoPort = 30002;
-    [SerializeField] private int peerVideoPort = 30006;
-    [SerializeField] private int myAudioPort = 30004;
-    [SerializeField] private int peerAudioPort = 30008;
-    [SerializeField] private int cameraNumber = 0;
-    
-    private int switchFlag = 0;
-    private bool videoStreamFlag = false;
-    private bool stringStreamFlag = false;
-    private bool endTcpConnect = false;
-    
     private void Start()
     {
-        // test
-        setCallback(DebugCallback, ReceiveTestVideo);
+        setMediaCallback(DebugCallback, ReceiveVideo, ReceiveAudio);
         setAddressAndPort(peerAddress, myTcpPort, peerTcpPort, myVideoPort, peerVideoPort, myAudioPort, peerAudioPort);
         setLibraryPath("./Assets/Plugins/CppConnect/libopenh264-2.3.1-mac-arm64.dylib");
-        test();
-        
+
         _mainThread = SynchronizationContext.Current;
         
         SetupUI();
@@ -209,15 +221,23 @@ public class UdpScript : MonoBehaviour
     // 停止時のクリーンアップ用の関数
     private async Task CleanupApplication()
     {
-        _disposable?.Dispose();
+        _sendVideoDisposable?.Dispose();
+        _sendAudioDisposable?.Dispose();
+        
         _receiveThread?.Abort();
         _tcpReceiveThread?.Abort();
+        
         if(_webCamTexture != null) _webCamTexture.Stop();
         if(_tcpCoroutine != null) StopCoroutine(_tcpCoroutine);
         
         if(switchFlag == 1 || stringStreamFlag) socketClose();
         // ビデオ送信開始してた場合
-        if(switchFlag == 2 || (videoStreamFlag && endTcpConnect)) destroyEncoder();
+        if (switchFlag == 2 || (videoStreamFlag && endTcpConnect))
+        {
+            // test
+            Microphone.End(_micName);
+            destroyEncoder();
+        }
         // ビデオ受信開始してた場合
         if(switchFlag == 3 || videoStreamFlag) await Task.Run(() =>
         {
@@ -336,7 +356,7 @@ public class UdpScript : MonoBehaviour
 
     /* ===========================================
      *
-     *         映像データの送受信をおこなう関数
+     *         映像・音声データの送受信をおこなう関数
      * 
      ========================================== */
     
@@ -353,30 +373,57 @@ public class UdpScript : MonoBehaviour
         Debug.Log("StartVideoStreamを開始");
         switchFlag = 2;
         endTcpConnect = true;
+        
+        // ビデオデータ用のセットアップ
         initEncodeVideoData(1);
-        // カメラのセットアップ
         var devices = WebCamTexture.devices;
         if (devices.Length <= 0) yield break;
-
         _webCamTexture = new WebCamTexture(devices[cameraNumber].name, 1920, 1080, 30);
         _webCamTexture.Play();
-
+        
         // 初期化が完了するまで待機
         while (_webCamTexture.width < 100) 
         {
             Debug.Log("カメラ初期化中");
             yield return null;
         }
-
         // バッファのサイズを設定
         _tmpBuffer = new Color32[_webCamTexture.width * _webCamTexture.height];
         _textureTmp = new Texture2D(_webCamTexture.width, _webCamTexture.height, TextureFormat.RGBA32, false);
         myVideoCapture.texture = _textureTmp;
+        
+        // 音声データ用のセットアップ
+        if (Microphone.devices.Length <= 0) yield break;
+        _micName = Microphone.devices[micNumber];
+        initEncodeAudioData();
+        _microphoneClip = Microphone.Start(_micName, true, 1, _micSampleRate);
+        
+        CheckMicrophoneSamplingRate();
 
-        _disposable.Disposable = Observable.Interval(TimeSpan.FromSeconds(1.0 / 30))
+        _sendVideoDisposable.Disposable = Observable.Interval(TimeSpan.FromSeconds(1.0 / 30))
             .Subscribe(_ => {
                 CallEncodeVideoData();
             });
+        
+        _sendAudioDisposable.Disposable = Observable.Interval(TimeSpan.FromSeconds(960.0 / 48000))
+            .Subscribe(_ => {
+                CallEncodeAudioData();
+            });
+    }
+
+    private void CheckMicrophoneSamplingRate()
+    {
+        Microphone.GetDeviceCaps(null, out var minFreq, out var maxFreq);
+        Debug.Log($"Minimum Frequency: {minFreq}, Maximum Frequency: {maxFreq}");
+        
+        if (_sampleSize != 960)
+        {
+            Debug.LogError($"Audio buffer size mismatch: {_sampleSize} != {960}");
+        }
+        else
+        {
+            Debug.Log("Audio buffer size is correct.");
+        }
     }
     
     // 30FPSで呼ばれる、カメラ映像をフレームごとに取得し、エンコードを実行する関数
@@ -393,6 +440,33 @@ public class UdpScript : MonoBehaviour
         // C++ のエンコード関数を呼び出し
         encodeVideoData(frameData,frameData.Length);
     }
+    
+    // (1.0 / 50)secで呼ばれる、音声データを取得し、エンコードを実行する関数
+    private void CallEncodeAudioData()
+    {
+        _microphonePosition = Microphone.GetPosition(null) - _sampleSize;
+
+        if (_microphonePosition < 0)
+            return;
+
+        _samples = new float[_sampleSize];
+        _microphoneClip.GetData(_samples, _microphonePosition);
+        
+        // float配列を16ビット整数に変換
+        short[] intData = new short[_samples.Length];
+        for (int i = 0; i < _samples.Length; i++)
+        {
+            intData[i] = (short)(_samples[i] * short.MaxValue);
+        }
+
+        // float配列をbyte配列に変換
+        _audioData = new byte[intData.Length * sizeof(short)];
+        Buffer.BlockCopy(intData, 0, _audioData, 0, _audioData.Length);
+
+        // C++の関数に音声データを渡す
+        encodeAudioData(_audioData, _audioData.Length);
+    }
+
 
     // カメラ映像の受信を開始する関数
     private void ReceiveTestStartVideoStream()
@@ -438,8 +512,15 @@ public class UdpScript : MonoBehaviour
 
     // 映像データ受信時にC++からコールバックされる関数
     [AOT.MonoPInvokeCallback(typeof(CallbackDelegate))]
-    public void ReceiveTestVideo(byte[] data, int size, int type)
+    public void ReceiveVideo(byte[] data, int size, int type)
     {
         _staticVideoSubject.OnNext(data);
+    }
+    
+    // 音声データ受信時にC++からコールバックされる関数
+    [AOT.MonoPInvokeCallback(typeof(CallbackDelegate))]
+    public void ReceiveAudio(byte[] data, int size, int type)
+    {
+        // _staticVideoSubject.OnNext(data);
     }
 }
